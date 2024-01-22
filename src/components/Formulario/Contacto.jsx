e8ba256a66299167241740330a51b1cc712a16fc
Sebastian Galeano <sebascarp9@gmail.com>
2024-01-22 11:15:59 -0300

    correcion emailjs

--- src/components/Formulario/Contacto.jsx
+++ src/components/Formulario/Contacto.jsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import emailjs from "@emailjs/browser";
+import emailjs from "emailjs";
 import "./Contacto.css";
 export function Contacto() {
   const form = useRef();
